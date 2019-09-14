d7ff7273fc831648e5657e7130f48641072772c0
Michael Rossi <michaelrossi@Michaels-MacBook-Pro.local>
2019-09-13 23:06:27 -0700

    The end of it all

--- routes/courts.js
+++ routes/courts.js
@@ -19,7 +19,11 @@ module.exports = function (app) {
         // Here we add an "include" property to our options in our findAll query
         // We set the value to an array of the models we want to include in a left outer join
         // In this case, just db.Post
-        db.Courts.findAll().then(function (courts) {
+        //Post.find({ where: { ...}, include: [User]})
+        
+        db.Courts.findAll({
+          include: [db.courtStatus]
+        }).then(function (courts) {
           res.json(courts);
         });
       }); 
@@ -40,9 +44,24 @@ module.exports = function (app) {
 
 
       app.post("/api/courts", function (req, res) {
+       
         db.Courts.create(req.body).then(function (dbCourts) {
           res.json(dbCourts);
+
+
+          // var myCourtStatus = {
+          //   comments: "No status yet",
+          //   numOfPlayers: 0,
+          //   lookingForMorePlayers: "",
+          //   CourtId: 1    // dbCourts.CourtId
+          // };
+          // db.courtStatus.create(myCourtStatus).then(function(dbCourtStatus) {
+          //   res.json(dbCourtStatus);
+          // });
+
+
         });
+        
       });
 };
 
